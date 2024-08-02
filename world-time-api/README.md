34b5624829f6aaab6d8ba58145fa3e87ceee5565
Kry9toN <kry9ton@kryptonproject.my.id>
2024-08-02 23:52:39 +0700

    add world time api

---
+++ world-time-api/README.md
@@ -0,0 +1,27 @@
+# World Time API
+
+[![Current Crates.io Version](https://img.shields.io/crates/v/world-time-api.svg)](https://crates.io/crates/world-time-api)
+[![Downloads badge](https://img.shields.io/crates/d/world-time-api.svg)](https://crates.io/crates/world-time-api)
+
+An API to retrieve date / time / daylight saving time informations about a timezone.
+
+Example:
+```
+curl http://localhost:8000/zoneinfo/Europe/Paris
+{"abbreviation":"CEST","datetime":"2019-10-08T20:16:13.612030900+02:00","dst_from":"2019-03-31T01:00:00Z","dst_offset":7200,"dst_period":true,"dst_until":"2019-10-27T01:00:00Z","raw_offset":3600,"timezone":"Europe/Paris","utc_datetime":"2019-10-08T18:16:13.612030900Z","utc_offset":"+02:00","week":40}
+```
+
+It uses system TZfiles (default location on Linux and Macos /usr/share/zoneinfo).
+You can override the TZfiles default location with the TZFILES_DIR environment variable (ending by a /).
+That also works on Windows, for example:
+
+```
+$env:TZFILES_DIR="C:\Users\nbauw\Dev\rs-tzfile\zoneinfo\"; cargo run
+```
+
+Some explanations about the offset fields:
+- raw_offset : the "normal" offset to utc, in seconds
+- dst_offset : the offset to utc during daylight saving time, in seconds
+- utc_offset : the current offset to utc, taking into account daylight saving time or not (according to dst_from and dst_until), in +/- HH:MM
+
+Provided dockerfile (which is no longer maintained) builds the app with statically compiled musl libc and then uses an alpine image to keep a low size.
---
+++ world-time-api/src/main.rs
@@ -0,0 +1,122 @@
+#[macro_use]
+extern crate rocket;
+
+use libtzfile::Tz;
+use rocket::{http::Status, http::RawStr, serde::json::Json};
+use std::env;
+use case_convert::CaseConvert;
+
+// Return a String that's correctly formatted for Timezone lookup.
+// Error if can't be parsed.
+//
+// We allow this to take types that can be converted into a String
+// ToDo - we could convert accented characters to non-accents.
+fn to_valid_format<S: Into<String>>(s: S) -> Result<String, String> {
+    let s: String = s.into();
+    let raw_str = RawStr::new(&s);
+    // We'll first have to make sure we convert any URI encoding to regular text.
+    // This means we URI decode the input, so "%20" becomes " ", etc.
+    // the uppercase_first method is called to convert for example europe/paris to proper Europe/Paris.
+    // We return the Decoded string as String, otherwise, return error
+    RawStr::percent_decode(raw_str)
+        .map(|s| s.to_string().uppercase_first().replace(" ", "_"))
+        .map_err(|_| String::from("Decoding error"))
+}
+
+// Return info on a Timezone.
+//
+// In the best case scenario, this returns the information for a requested Timezone.
+//
+// If neither the region or city match a known Timezone, we return a 404, which
+// is done via Ok(None).
+//
+// If we do find a valid region and city, then we return that data as a Josn
+// object via Ok(Some) with a 200.
+//
+// If the requested Timezone has bad formatting, then we want to return an
+// error specifying that this is a 400, which we do via an Err.
+#[get("/<region>/<city>")]
+fn get_tzinfo(region: &str, city: &str) -> Result<Option<Json<libtzfile::Tzinfo>>, Status> {
+    // If both geo-location values can be parsed...
+    if let (Ok(region), Ok(city)) = (to_valid_format(region), to_valid_format(city)) {
+        // TZfiles location can be customized through the TZFILES_DIR environment. Default location is /usr/share/zoneinfo.
+        let mut tz_file = String::new();
+        tz_file.push_str(&env::var("TZFILES_DIR").unwrap_or(format!("/usr/share/zoneinfo/")));
+        // Then we create a String using format to create a Timezone lookup key.
+        let s = if cfg!(windows) {
+            format!("{}\\{}", region, city)
+        } else {
+            format!("{}/{}", region, city)
+        };
+        tz_file.push_str(&s);
+
+        // We run `.ok` to convert from Result to Option.
+        // `and_then` lets us work directly with the values.
+        // If it encounters a problem, then it returns None.
+        // No matter if we get a Some or None, we wrap it in Ok.
+        // We do this because Rocket can handle None as a 404.
+        // The Tzinfo struct implements the Serialize trait.
+        let tz = Tz::new(&tz_file)
+            .and_then(|z| z.zoneinfo())
+            .ok()
+            .map(Json);
+        Ok(tz)
+    } else {
+        // If both geo-location values can't be parsed, return a BadRequest
+        Err(Status::BadRequest)
+    }
+}
+
+#[get("/<timezone>")]
+fn get_standardtime(timezone: &str) -> Result<Option<Json<libtzfile::Tzinfo>>, Status> {
+    // If timezone can be parsed...
+    if let Ok(timezone) = to_valid_format(timezone) {
+        // TZfiles location can be customized through the TZFILES_DIR environment. Default location is /usr/share/zoneinfo.
+        let mut tz_file = String::new();
+        tz_file.push_str(&env::var("TZFILES_DIR").unwrap_or(format!("/usr/share/zoneinfo/")));
+        tz_file.push_str(&timezone);
+
+        let tz = Tz::new(&tz_file)
+            .and_then(|z| z.zoneinfo())
+            .ok()
+            .map(Json);
+        Ok(tz)
+    } else {
+        // If both geo-location values can't be parsed, return a BadRequest
+        Err(Status::BadRequest)
+    }
+}
+
+#[get("/")]
+fn get_root() -> Result<Option<Json<libtzfile::Tzinfo>>, Status> {
+    Err(Status::BadRequest)
+}
+
+// Error Catchers
+
+// It's nice to have this come back as Json.
+// Since we're working with a known str value, we can specify it as static.
+// Removed rocket::Request variable since we aren't using it.
+#[catch(400)]
+fn bad_request() -> Json<&'static str> {
+    Json("Invalid Timezone")
+}
+// Again, nice to have Json.
+#[catch(404)]
+fn not_found<'a>(req: &'a rocket::Request) -> Json<String> {
+    Json(format!("Unable to find Timezone {}", req.uri().path()))
+}
+
+// Again, nice to have Json.
+#[catch(404)]
+fn invalid_path() -> Json<String> {
+    Json(format!("Invalid path"))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/zoneinfo", routes![get_tzinfo, get_standardtime, get_root])
+        .register("/zoneinfo", catchers![bad_request, not_found])
+        .register("/", catchers![invalid_path])
+}
